8bf6c602fc35261023742d79c6717e0723b712c8
Leandro Macedo <leeandromacedo117@gmail.com>
2024-01-02 10:19:42 -0300

    upload App

---
+++ quiz-app/src/componentes/page_main/api/api.jsx
@@ -0,0 +1,25 @@
+// DataFetchingComponent.jsx
+import { useState, useEffect } from 'react';
+import api from '../../../../api';
+
+const DataFetchingComponent = () => {
+  const [dataFromApi, setDataFromApi] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await api.get('/test');
+        setDataFromApi(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.error("Houve um erro: " + error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return dataFromApi; // Retornando os dados obtidos da API
+};
+
+export default DataFetchingComponent;
