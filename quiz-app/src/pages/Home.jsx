bc0be327bf494cd61d15ca7053fe5422479f7884
Leandro Macedo <leeandromacedo117@gmail.com>
2024-01-24 16:18:39 -0300

    Sql config Aqui estão as configurações necessaria para o banco de dados. No pull request fiz adicionei uma novo hooks para o Login de usuarios, no momentos apenas ignore esse hook

--- quiz-app/src/pages/Home.jsx
+++ quiz-app/src/pages/Home.jsx
@@ -2,11 +2,12 @@ import '../App.css';
 import Todo from '../componentes/Todo';
 import Day from '../componentes/day'
 import useItervalReturn from '../hooks/useIntervalReturn';
-
+// import UseloginAntentication from '../hooks/useloginAntentication';
 function Home() {
 
   return (
     <div className="App">
+      {/* <UseloginAntentication></UseloginAntentication> */}
       <header className="time-day">
         <div className="container-day">
           <Day></Day>
