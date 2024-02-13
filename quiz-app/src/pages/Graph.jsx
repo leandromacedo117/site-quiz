d1d3a71ab2c27f372bb85565d3c67e6260515871
Caio Henrique <Caio_Henrique2006@outlook.com>
2024-02-13 10:58:27 -0300

    add chart.js

--- quiz-app/src/pages/Graph.jsx
+++ quiz-app/src/pages/Graph.jsx
@@ -1,7 +1,38 @@
+import { Line } from 'react-chartjs-2';
+import { CategoryScale, Chart, LinearScale, PointElement, LineElement } from "chart.js";
+import "../style/Graph.css"
+
+Chart.register(CategoryScale, LinearScale, PointElement, LineElement);
+
+
 function Graph() {
+
+  const data = {
+      labels: ["a", "b", "c", "d", "e", "f", "g"],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }]
+  }
+
   return (
     <center>
       <h1>Página dos gráficos</h1>
+      <div className="chart">
+        <Line
+          options={{
+            type: 'line',
+            data: data,
+            maintainAspectRatio: false
+          }}
+          width={500}
+          height={500}
+          data={data}
+        />
+      </div>
     </center>
   )
 }
